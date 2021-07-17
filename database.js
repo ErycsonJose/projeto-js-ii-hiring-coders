c387c278c76ee2f20e23a9c86c5e7278fff9ab99
ErycsonJose <erycsonjose@gmail.com>
2021-07-16 22:27:25 -0300

    subindo arquivos para github

---
+++ database.js
@@ -0,0 +1,58 @@
+const livros =[
+    {
+        id:1,
+        nome: "Torto Arado" ,
+        autor: "Itamar Vieira Junior",
+        categoria: "Romance",
+        paginas: 239,
+        recomenda: false,
+        leu: false,
+    },
+    {
+        id:2,
+        nome: "Leonardo da Vinci" ,
+        autor: "Walter Isaacson",
+        categoria: "Biografia",
+        paginas: 640,
+        recomenda: true,
+        leu: false,
+    },
+    {
+        id:3,
+        nome: "A Guerra dos Tronos" ,
+        autor: "George R. R. Martin",
+        categoria: "Ficção",
+        paginas: 522,
+        recomenda: true,
+        leu: true,
+    },
+    {
+        id:4,
+        nome: "Hábitos Atómicos" ,
+        autor: "James Clear",
+        categoria: "Auto-ajuda",
+        paginas: 209,
+        recomenda: true,
+        leu: false,
+    },
+    {
+        id:5,
+        nome: "As leis do triunfo e sucesso de Napoleon Hill" ,
+        autor: "Napoleon Hill",
+        categoria: "Auto-ajuda",
+        paginas: 784,
+        recomenda: true,
+        leu: true,
+    },
+    {
+        id:6,
+        nome: "Perverta-me" ,
+        autor: "Anna Zaires",
+        categoria: "Romance",
+        paginas: 812,
+        recomenda: false,
+        leu: true,
+    }
+]
+
+module.exports = livros
